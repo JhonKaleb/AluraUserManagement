4c6a708e10041342ad01320fbb1e412ce1c69b91
Jhon Kaleb <jhon.kaleb@hotmail.com>
2024-03-23 13:50:46 -0300

    fix(user): Added exception folder at component scan

--- src/main/java/com/alura/UserManagement/UserManagementApplication.java
+++ src/main/java/com/alura/UserManagement/UserManagementApplication.java
@@ -15,6 +15,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 		"com.alura.UserManagement.service",
 		"com.alura.UserManagement.repository",
 		"com.alura.UserManagement.controller",
+		"com.alura.UserManagement.exception",
 })
 @EntityScan(basePackages = {"com.alura.UserManagement.domain"})
 @EnableJpaRepositories({"com.alura.UserManagement.repository"})
